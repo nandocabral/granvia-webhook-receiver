5fd643797668eec7f5b945082ccdf351b1109dba
Hernando Cabral <Hernando.Cabralmedina@syngenta.com>
2023-09-22 12:19:05 -0600

    adding validation to webhook

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,38 +9,38 @@ import {
   Req,
 } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as crypto from 'crypto';
+import { throwError } from 'rxjs';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('shopify/webhook/orders/:store')
-  readHook(
+  async readHook(
     @Param('store') store: string,
     @Req() req: RawBodyRequest<Request>,
     @Body() body: any,
     @Headers() headers: any,
   ) {
-    // const webhooks = {
-    //   test: process.env.SHOPIFY_SECRET,
-    //   granvia: process.env.SHOPIFY_SECRET_GRANVIA,
-    //   granviaoutlet: process.env.SHOPIFY_SECRET_GRANVIA_OUTLET,
-    //   louder: process.env.SHOPIFY_SECRET_GRANVIA_LOUDER,
-    // };
-    // const secret = webhooks[store];
+    const webhooks = {
+      test: process.env.SHOPIFY_SECRET,
+      granvia: process.env.SHOPIFY_SECRET_GRANVIA,
+      granviaoutlet: process.env.SHOPIFY_SECRET_GRANVIA_OUTLET,
+      louder: process.env.SHOPIFY_SECRET_GRANVIA_LOUDER,
+    };
+    const secret = webhooks[store];
     const shopifyHash = headers['x-shopify-hmac-sha256'];
-    console.log(
-      `Controller: ${body.id}, made in: ${store} with hash: ${shopifyHash}`,
-    );
-    // const hash = crypto
-    //   .createHmac('sha256', secret)
-    //   .update(req.rawBody)
-    //   .digest('base64');
-    // console.log(shopifyHash, hash);
-    // if (hash !== shopifyHash) {
-    //   return throwError(() => 'Da hell are you doing here!');
-    // }
-    // return this.shopifyService.readHook(body, store);
+    console.log(`Controller: ${body.id}, made in: ${store}`);
+    const hash = crypto
+      .createHmac('sha256', secret)
+      .update(req.rawBody)
+      .digest('base64');
+    console.log(shopifyHash, hash);
+    if (hash !== shopifyHash) {
+      return throwError(() => 'Da hell are you doing here!');
+    }
+    return this.appService.setWebhook(body, store, secret);
   }
 
   @Get()
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  constructor(private httpService: HttpService) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async setWebhook(body: any, store: string, secret: string) {
+    try {
+      const { data } = await firstValueFrom(
+        this.httpService.post(
+          `${process.env.URL_SITE}/api/v1/webhook/shopify/public/public/orders/${store}`,
+          { body, secret },
+        ),
+      );
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
