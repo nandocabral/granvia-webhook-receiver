e9678b1956f2bece668b7b7a9a0afede2f2ac258
Hernando Cabral <Hernando.Cabralmedina@syngenta.com>
2023-10-07 09:24:07 -0600

    adding new endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -16,6 +16,11 @@ import { throwError } from 'rxjs';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
   @Post('shopify/webhook/orders/:store')
   async readHook(
     @Param('store') store: string,
@@ -42,8 +47,29 @@ export class AppController {
     return this.appService.setWebhook(body, store, secret);
   }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('shopify/webhook/orders/edit/:store')
+  async readUpdateHook(
+    @Param('store') store: string,
+    @Req() req: RawBodyRequest<Request>,
+    @Body() body: any,
+    @Headers() headers: any,
+  ) {
+    const webhooks = {
+      test: process.env.SHOPIFY_SECRET,
+      granvia: process.env.SHOPIFY_SECRET_GRANVIA,
+      granviaoutlet: process.env.SHOPIFY_SECRET_GRANVIA_OUTLET,
+      louder: process.env.SHOPIFY_SECRET_GRANVIA_LOUDER,
+    };
+    const secret = webhooks[store];
+    const shopifyHash = headers['x-shopify-hmac-sha256'];
+    console.log(`Controller: ${body.id}, made in: ${store}`);
+    const hash = crypto
+      .createHmac('sha256', secret)
+      .update(req.rawBody)
+      .digest('base64');
+    if (hash !== shopifyHash) {
+      return throwError(() => 'Da hell are you doing here!');
+    }
+    return this.appService.readUpdateHook(body, store, secret);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,43 @@ import axios from 'axios';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `
+    <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            rel="icon"
+            href="https://granvia.mx/cdn/shop/files/gv_32x.png?v=1668022368"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Oswald&display=swap"
+            rel="stylesheet"
+          />
+          <title>GRANVIA</title>
+          <style>
+            body {
+              font-family: "Oswald", sans-serif;
+            }
+          </style>
+        </head>
+        <body
+          style="
+            background-color: black;
+            height: 100vh;
+            width: 100vw;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          "
+        >
+          <h1 style="color: white">GRANVIA PROXY SERVICE OK</h1>
+        </body>
+      </html>
+    `;
   }
 
   setWebhook(body: any, store: string, secret: string) {
@@ -12,6 +48,18 @@ export class AppService {
       process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
       axios.post(
         `${process.env.URL_SITE}/api/v1/webhook/shopify/public/public/orders/${store}`,
+        { body, secret },
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  readUpdateHook(body: any, store: string, secret: string) {
+    try {
+      process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+      axios.post(
+        `${process.env.URL_SITE}/api/v1/webhook/shopify/public/public/orders/edit/${store}`,
         { body, secret },
       );
     } catch (error) {
